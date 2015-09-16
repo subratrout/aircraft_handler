3a051cce5b65f513e57c075aca30ef7c0d9fbbe5
Subrat Rout <subratnrout@gmail.com>
2015-09-15 20:29:39 -0700

    Added index, new and create methods to aircraftsController

--- app/controllers/aircrafts_controller.rb
+++ app/controllers/aircrafts_controller.rb
@@ -1,2 +1,27 @@
 class AircraftsController < ApplicationController
+
+  def index
+    @aircrafts = Aircraft.all
+  end
+
+  def new
+    @aircraft = Aircraft.new
+  end
+
+  def create
+    @aircraft = Aircraft.new(aircraft_params)
+    if @aircraft.save
+      redirect_to root_path, notice: "Aircraft  was added to the queue."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def aircraft_params
+    params.require(:aircraft).permit(:aircraft_type, :aircraft_size)
+  end
+
+
 end
--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -1,2 +1,12 @@
 class Aircraft < ActiveRecord::Base
+
+  validates :aircraft_type, :aircraft_size, presence: true
+
+  # List type of aircrafts
+  AIRCRAFT_TYPES = ["cargo", "passenger"]
+
+  # List size of aircrafts
+  AIRCRAFT_SIZES = ["small", "large"]
+
+
 end
